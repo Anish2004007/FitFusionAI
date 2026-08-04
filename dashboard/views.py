f855bdd339eebd87c0b23f4f8d735f05ea0f638f
your-github-username <your-email@example.com>
2026-08-04 22:16:37 +0530

    Refactor dashboard structure and implement premium dashboard UI

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from accounts.models import User
+
 
 def dashboard(request):
-    return render(request, "dashboard/dashboard.html")
+
+    user_id = request.session.get("user_id")
+
+    if not user_id:
+
+        return redirect("login")
+
+    user = User.objects.get(user_id=user_id)
+
+    return render(
+        request,
+        "dashboard/dashboard.html",
+        {
+            "user": user
+        }
+    )
